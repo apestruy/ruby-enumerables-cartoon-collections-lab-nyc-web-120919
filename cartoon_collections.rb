54930a7c697c71542a745612495d1f4c92a5ea7c
Alexandra Pestruyeva <apestruy@gmail.com>
2019-12-06 03:31:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find { |cheese| cheese.include?(cheese_types) }
+  array.find { |cheese| cheese.include?(cheese_types) == true }
   return cheese 
 end
